d07fdb6e4cd51a1a90bacdc72ff8416b5dd2c9c5
kasian77 <79701896+kasian77@users.noreply.github.com>
2021-03-28 10:20:34 +0300

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,6 +1,12 @@
-<?php 
-    $arr = [1,2,3,4,5,6,7,8,9];
-    
-    
-    echo array_sum($arr) / count($arr);
-?>
+<?php
+$arr = [
+	"Dan" => "1978",
+	"John" => "2002",
+	"Ryan" => "1930"
+];
+ksort($arr);
+print_r($arr);
+asort($arr);
+print_r($arr);
+sort($arr);
+print_r($arr);
