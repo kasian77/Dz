decf46f2e1be5e5dee38e3677d326faca9811341
kasian77 <79701896+kasian77@users.noreply.github.com>
2021-03-28 10:16:54 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,6 +1,15 @@
 <?php
-    $arr = [3, 5, 100, -5, 20];
-    $min = min($arr);
-    $max = max($arr);
-    $sum = min($arr) +max($arr);
-    echo $sum;
+$arr = [
+    [1, -2, 3],
+    [-4, 5, -6],
+    [-7, -8, 10]
+];
+$sum = 0;
+for ($i = 0; $i < count($arr); $i++) {
+    for ($j = 0; $j < count($arr); $j++) {
+        if ($arr[$i][$j] > 0) {
+            $sum += $arr[$i][$j];
+        }
+    }
+}
+echo $sum;
